5a954a724ee3157d3c807f6a64eecf0e3032bb68
chioJG <zkxmfkdlej5454@gmail.com>
2025-04-10 16:13:03 +0900

    login

---
+++ app/src/main/java/com/example/loginpage/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.loginpage;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    private ImageView imageView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        ImageView imageview = findViewById(R.id.imageView2);
+        imageview.setImageResource(R.drawable.android);
+    }
+}
